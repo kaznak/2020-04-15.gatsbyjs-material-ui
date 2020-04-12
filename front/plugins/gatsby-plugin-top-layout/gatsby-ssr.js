4f13028b914d4e66ef1f069df63e2dffb6938403
kaznak <kaznak.at.work@gmail.com>
2020-04-12 14:50:33 +0900

    change source tree

--- front/plugins/gatsby-plugin-top-layout/gatsby-ssr.js
+++ front/plugins/gatsby-plugin-top-layout/gatsby-ssr.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/prefer-default-export, react/prop-types */
 import React from "react"
-import TopLayout from "./TopLayout"
+import TopLayout from "./src/component/TopLayout"
 
 export const wrapRootElement = ({ element }) => {
   return <TopLayout>{element}</TopLayout>
