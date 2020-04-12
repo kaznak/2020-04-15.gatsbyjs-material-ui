a4563b14667c0c21202561f8c283fdf0f8cbc9ff
kaznak <kaznak.at.work@gmail.com>
2020-04-12 22:32:28 +0900

    change path

--- front/src/pages/index.js
+++ front/src/pages/index.js
@@ -2,9 +2,9 @@ import React from "react"
 import Container from "@material-ui/core/Container"
 import Typography from "@material-ui/core/Typography"
 import Box from "@material-ui/core/Box"
-import ProTip from "../components/ProTip"
+import ProTip from "../components/app/ProTip"
 import Link from "../components/core/Link"
-import Copyright from "../components/Copyright"
+import Copyright from "../components/app/Copyright"
 
 export default function Index() {
   return (
